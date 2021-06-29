2438233d121b3319e076bd34349545fae3c246c0
Jim Carpenter <45828944+jcarp19@users.noreply.github.com>
2021-06-29 15:03:35 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+document.querySelector(".clickMe").addEventListener("click", () => {
+    // document.querySelector(".box").style.backgroundColor = "red";
+    document.querySelector(".box").classList.add("moveTo");
+    
+});
